2d6ba69f22e324053e1841c8bccc41421881ac35
Obairoko <140971160+Obairoko@users.noreply.github.com>
2023-07-31 10:38:27 -0400

    Add files via upload

---
+++ bucle3.py
@@ -0,0 +1,9 @@
+for i in range(1, 11):
+    print(i)
+    
+     
+    
+    '''Este bucle imprime los numeros del 1 al 10.'''
+    '''Integrantes: Yenderson Teran, Ducey Morales y Dayeixi Sanchez.'''
+   
+ 
